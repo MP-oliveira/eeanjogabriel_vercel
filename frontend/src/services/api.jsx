7e429c1f5e81706606fd8e11d1b0ddb8a447f198
Mauricio Oliveira <69546564+MP-oliveira@users.noreply.github.com>
2025-05-31 14:46:50 -0300

    Update api.jsx
    feat: add url in api.jsx

--- frontend/src/services/api.jsx
+++ frontend/src/services/api.jsx
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 // Configurando a URL base do backend
 const api = axios.create({
-  baseURL: 'https://back-eeanjogabriel-vercel.vercel.app/api',
+  baseURL: 'https://back-eeanjogabriel-vercel-nine.vercel.app/api',
   withCredentials: true,
   headers: {
     'Content-Type': 'application/json'
